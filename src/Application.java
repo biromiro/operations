97011bcef047266c93f68ab4574520160f32eba0
Bruno Mendes <brod56@sapo.pt>
2021-02-17 11:40:56 +0000

    Add calculator

--- src/Application.java
+++ src/Application.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Calculate the sum of two numbers\n");
+        System.out.println("Calculate the sum/mul of two numbers\n");
 
         System.out.print("Please input the first number: ");
         int a = scanner.nextInt();
         System.out.print("Please input the second number: ");
         int b = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.format("The sum is equal to %d", a + b);
+        for(;;){
+            System.out.print("Please input the desired operation (sum/mul): ");
+            String op = scanner.nextLine();
+            if (op.equals("mul")){
+                System.out.format("The mul is equal to %d\n", a * b);
+                break;
+            }
+            else if (op.equals("sum")){
+                System.out.format("The sum is equal to %d\n", a + b);
+                break;
+            }
+            else {
+                System.out.print("Read carefully!!\n");
+            }
+        }
     }
 }
